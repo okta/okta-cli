0c7b21ef57dec235ff16d126fff1888790e77679
Brian Demers <bdemers@apache.org>
2020-05-12 13:09:26 -0400

    Add install script to pipe into bash
    This needs to be cleaned up and zip replaced with tar

---
+++ cli/src/main/scripts/install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+{
+    set -e
+    SUDO=''
+    if [ "$(id -u)" != "0" ]; then
+      SUDO='sudo'
+      echo "This script requires superuser access."
+      echo "You will be prompted for your password by sudo."
+      # clear any previous sudo permission
+      sudo -k
+    fi
+
+
+    # run inside sudo
+    $SUDO bash <<SCRIPT
+  set -e
+
+  LINUX_DIST="https://raw.githubusercontent.com/oktadeveloper/okta-maven-plugin/cli-test-dist/okta-cli-ubuntu-latest-x86_64.zip"
+  DARWIN_DIST="https://raw.githubusercontent.com/oktadeveloper/okta-maven-plugin/cli-test-dist/okta-cli-macos-latest-x86_64.zip"
+
+  echoerr() { echo "\$@" 1>&2; }
+
+  if [[ ! ":\$PATH:" == *":/usr/local/bin:"* ]]; then
+    echoerr "Your path is missing /usr/local/bin, you need to add this to use this installer."
+    exit 1
+  fi
+
+  if [ "\$(uname)" == "Darwin" ]; then
+    OS=darwin
+    URL=\$DARWIN_DIST
+  elif [ "\$(expr substr \$(uname -s) 1 5)" == "Linux" ]; then
+    OS=linux
+    URL=\$LINUX_DIST
+  else
+    echoerr "This installer is only supported on Linux and MacOS"
+    exit 1
+  fi
+
+  ARCH="\$(uname -m)"
+  if [ "\$ARCH" == "x86_64" ]; then
+    ARCH=x64
+  else
+    echoerr "unsupported arch: \$ARCH"
+    exit 1
+  fi
+
+  mkdir -p /usr/local/lib/okta/bin
+  cd /usr/local/lib/okta/bin
+  rm -rf okta
+
+  echo "Installing CLI from \$URL"
+  if [ \$(command -v curl) ]; then
+    curl "\$URL" | funzip > okta
+  else
+    wget -O- "\$URL" | funzip > okta
+  fi
+
+  chmod 755 okta
+
+  # delete old heroku bin if exists
+  rm -f \$(command -v okta) || true
+  rm -f /usr/local/bin/okta
+  ln -s /usr/local/lib/okta/bin/okta /usr/local/bin/okta
+
+SCRIPT
+  # test the CLI
+  LOCATION=$(command -v okta)
+  echo "okta installed to $LOCATION"
+  okta --version
+}
