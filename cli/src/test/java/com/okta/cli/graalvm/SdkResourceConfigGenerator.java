9b3880c96c31a31cc98399a802508896a0bce0a8
Brian Demers <bdemers@apache.org>
2020-06-01 17:03:01 -0400

    Revert "Upgrading to Graalvm 20.1.0"
    This reverts commit 497c3fa208d79aea5c33bf818a1c871e18b724d1.

--- cli/src/test/java/com/okta/cli/graalvm/SdkResourceConfigGenerator.java
+++ cli/src/test/java/com/okta/cli/graalvm/SdkResourceConfigGenerator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.okta.cli.graal;
+package com.okta.cli.graalvm;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.reflect.ClassPath;
